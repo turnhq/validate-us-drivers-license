dd6fdd3dcd1fe6b879fbe2f99e1d14daeb50336b
David Cook <david@turning.io>
2022-03-10 10:08:10 -0600

    updated regexs

--- src/license-regex.js
+++ src/license-regex.js
@@ -12,408 +12,419 @@ module.exports = {
   AL: [
     {
       regex: /^\d{7,8}$/,
-      description: 'NNNNNNNNN MinLength = 7 MaxLength = 8'
+      description: "NNNNNNNNN MinLength = 7 MaxLength = 8",
     },
     {
       regex: /^[A-Z]\d{6,7}$/i,
-      description: 'ANNNNNNNN MinLength = 7 MaxLength = 8'
+      description: "ANNNNNNNN MinLength = 7 MaxLength = 8",
     },
   ],
   AK: [
     {
       regex: /^\d{7}$/,
-      description: 'NNNNNNNN MinLength = 7 MaxLength = 7',
+      description: "NNNNNNNN MinLength = 7 MaxLength = 7",
     },
   ],
   AZ: [
     {
       regex: /^[A-Z]\d{8}$/i,
-      description: 'ANNNNNNNN MinLength = 9 MaxLength = 9',
+      description: "ANNNNNNNN MinLength = 9 MaxLength = 9",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   AR: [
     {
       regex: /^[0-9]{9}$/,
-      description: 'NNNNNNNNN MinLength = 9 MaxLength = 9',
+      description: "NNNNNNNNN MinLength = 9 MaxLength = 9",
     },
     {
       regex: /^[A-Z]\d{8}$/i,
-      description: 'ANNNNNNNN MinLength = 9 MaxLength = 9',
+      description: "ANNNNNNNN MinLength = 9 MaxLength = 9",
     },
   ],
   CA: [
     {
       regex: /^[A-Z]{1}[0-9]{7}$/i,
-      description: 'License must be 1 letter followed by 7 numbers',
+      description: "License must be 1 letter followed by 7 numbers",
     },
   ],
   CO: [
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
+    },
+    {
+      regex: /^[A-Z]{1}[0-9]{3,6}$/i,
+      description: "ANNNNNN min length 4 max 7",
+    },
+    {
+      regex: /^[A-Z]{2}[0-9]{1,5}$/i,
+      description: "AANNNNN min length 3 max 7",
     },
   ],
   CT: [
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   DE: [
     {
       regex: /^\d{3,7}$/,
-      description: 'License must be 3-7 numbers',
+      description: "License must be 3-7 numbers",
     },
   ],
   DC: [
     {
       regex: /^[0-9]{7}$/i,
-      description: 'License must be 7 numbers',
+      description: "License must be 7 numbers",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
-    }
+      description: "License must be 9 numbers",
+    },
   ],
   FL: [
     {
       regex: /^[A-Z]{1}[0-9]{12}$/i,
-      description: 'License must be 1 letter followed by 12 numbers',
+      description: "License must be 1 letter followed by 12 numbers",
     },
     {
       regex: /^[A-Z]\d{11}$/i,
-      description: 'ANNNNNNNNNNN MinLength = 12 MaxLength = 12',
+      description: "ANNNNNNNNNNN MinLength = 12 MaxLength = 12",
     },
   ],
   GA: [
     {
       regex: /^[0-9]{7,9}$/i,
-      description: 'License must be 7-9 numbers',
+      description: "License must be 7-9 numbers",
     },
   ],
   GU: [
     {
       regex: /^\d{10}$/,
-      description: 'NNNNNNNNNN MinLength = 10 MaxLength = 10',
+      description: "NNNNNNNNNN MinLength = 10 MaxLength = 10",
     },
   ],
   HI: [
     {
       regex: /^[A-Z]{1}[0-9]{8}$/i,
-      description: 'License must be 1 letter followed by 8 numbers'
+      description: "License must be 1 letter followed by 8 numbers",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers'
-    }
+      description: "License must be 9 numbers",
+    },
   ],
   ID: [
     {
       regex: /^[A-Z]{2}[0-9]{6}[A-Z]{1}$/i,
-      description: 'License must be 2 letters followed by 6 numbers followed by 1 letter',
+      description:
+        "License must be 2 letters followed by 6 numbers followed by 1 letter",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   IL: [
     {
       regex: /^[A-Z]{1}[0-9]{11}$/i,
-      description: 'License must be 1 letter followed by 11 numbers',
-    }
+      description: "License must be 1 letter followed by 11 numbers",
+    },
   ],
   IN: [
     {
       regex: /^[0-9]{9,10}$/i,
-      description: 'License must be 9-10 numbers',
+      description: "License must be 9-10 numbers",
     },
   ],
   IA: [
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
     {
       regex: /^[0-9]{3}[A-Z]{2}[0-9]{4}$/i,
-      description: 'License must be 3 numbers followed by 2 letters followed by 4 numbers',
+      description:
+        "License must be 3 numbers followed by 2 letters followed by 4 numbers",
     },
   ],
   KS: [
     {
       regex: /^[A-Z]{1}[0-9]{8}$/i,
-      description: 'License must be 1 letter followed by 8 numbers',
+      description: "License must be 1 letter followed by 8 numbers",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   KY: [
     {
       regex: /^[A-Z]{1}[0-9]{8,9}$/i,
-      description: 'License must be 1 letter followed by 8-9 numbers',
+      description: "License must be 1 letter followed by 8-9 numbers",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   LA: [
     {
       regex: /^[0-9]{1,9}$/i,
-      description: 'License must be 1-9 numbers'
+      description: "License must be 1-9 numbers",
     },
   ],
   ME: [
     {
       regex: /^[0-9]{7}$/i,
-      description: 'License must be 7 numbers',
+      description: "License must be 7 numbers",
     },
     {
       regex: /^[0-9]{7}[A-Z]{1}$/i,
-      description: 'License must be 7 numbers followed by 1 letter',
+      description: "License must be 7 numbers followed by 1 letter",
     },
     {
       regex: /^[0-9]{8}$/i,
-      description: 'License must be 8 numbers',
+      description: "License must be 8 numbers",
     },
   ],
   MD: [
     {
-      regex: /^[A-Z]{1}[0-9]{12}$/i,
-      description: 'License must be 1 letter followed by 12 numbers',
+      regex: /^[A-Z]{1,2}[0-9]{11,12}$/i,
+      description:
+        "License must be 1 or 2 letters followed by 11 or 12 numbers",
     },
   ],
   MA: [
     {
       regex: /(^[A-Z]{1}[0-9]{8}$|^[A-Z]{2}[0-9]{7}$)/i,
-      description: 'License must be 1 letter followed by 8 numbers or 2 letters followed by 7 numbers',
+      description:
+        "License must be 1 letter followed by 8 numbers or 2 letters followed by 7 numbers",
     },
   ],
   MI: [
     {
       regex: /^[A-Z]{1}[0-9]{12}$/i,
-      description: 'License must be 1 letter followed by 12 numbers',
+      description: "License must be 1 letter followed by 12 numbers",
     },
   ],
   MN: [
     {
       regex: /^[A-Z]{1}[0-9]{12}$/i,
-      description: 'License must be 1 letter followed by 12 numbers'
+      description: "License must be 1 letter followed by 12 numbers",
     },
   ],
   MS: [
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   // TODO: Test remaining Licenses
   MO: [
     {
       regex: /^[A-Z]{1}[0-9]{5,9}$/i,
-      description: 'License must be 1 letter followed by 5-9 numbers',
+      description: "License must be 1 letter followed by 5-9 numbers",
     },
     {
       regex: /^[A-Z]{1}[0-9]{6}[R]{1}$/i,
-      description: 'License must be 1 letter followed by 6 numbers followed by "R"',
+      description:
+        'License must be 1 letter followed by 6 numbers followed by "R"',
     },
     {
       regex: /^[0-9]{9}[A-Z]{1}$/i,
-      description: 'License must be 9 numbers followed by 1 letter',
+      description: "License must be 9 numbers followed by 1 letter",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
     {
       regex: /^[0-9]{3}[A-Z]{1}[0-9]{6}$/i,
-      description: 'License must be 3 numbers + 1 alfanumeric + 6 numbers',
+      description: "License must be 3 numbers + 1 alfanumeric + 6 numbers",
     },
     {
       regex: /^[A-Z]\d{5,6}[A-Z]$/i,
-      description: 'ANNNNNA MinLength = 7 MaxLength = 7 OR ANNNNNA MinLength = 8 MaxLength = 8'
+      description:
+        "ANNNNNA MinLength = 7 MaxLength = 7 OR ANNNNNA MinLength = 8 MaxLength = 8",
     },
   ],
   MT: [
     {
       regex: /^[0-9]{13}$/i,
-      description: 'License must be 13 numbers',
+      description: "License must be 13 numbers",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
     {
       regex: /^[A-Z]\d(\d|[A-Z])\d{2}[A-Z]{3}\d$/i,
-      description: 'ANXNNAAAN MinLength = 9 MaxLength = 9'
+      description: "ANXNNAAAN MinLength = 9 MaxLength = 9",
     },
     {
       regex: /^[A-Z]\d(\d|[A-Z])\d{2}[A-Z]{3}\d$/i,
-      description: 'ANXNNAAAN MinLength = 9 MaxLength = 9'
+      description: "ANXNNAAAN MinLength = 9 MaxLength = 9",
     },
     {
       regex: /^[a-z]{3}\d{10}$/i,
-      description: 'Three letters and 10 digits',
+      description: "Three letters and 10 digits",
     },
   ],
   NE: [
     {
       regex: /^[A-Z]\d{3,8}$/i,
-      description: 'ANNNNNNNN MinLength = 4 MaxLength = 9'
+      description: "ANNNNNNNN MinLength = 4 MaxLength = 9",
     },
   ],
   NV: [
     {
       regex: /^[0-9]{9,10}$/i,
-      description: 'License must be 9-10 numbers',
+      description: "License must be 9-10 numbers",
     },
     {
       regex: /^[0-9]{12}$/i,
-      description: 'License must be 12 numbers',
-    },
-    {
-      regex: /^[X]{1}[0-9]{8}$/i,
-      description: 'License must be "X" followed by 8 numbers',
+      description: "License must be 12 numbers",
     },
     {
       regex: /^[A-Z]\d{8}$/i,
-      description: 'ANNNNNNNN MinLength = 9 MaxLength = 9'
+      description: "ANNNNNNNN MinLength = 9 MaxLength = 9",
     },
   ],
   NH: [
     {
       regex: /(^[0-9]{2}[A-Z]{3}[0-9]{5}$|^[A-Z]{3}[0-9]{8}$)/i,
-      description: 'License must be 2 numbers followed by 3 letters followed by 5 numbers OR 3 letters followed by 8 numbers'
+      description:
+        "License must be 2 numbers followed by 3 letters followed by 5 numbers OR 3 letters followed by 8 numbers",
     },
   ],
   NJ: [
     {
       regex: /^[A-Z]{1}[0-9]{14}$/i,
-      description: 'License must be 1 letter followed by 14 numbers',
+      description: "License must be 1 letter followed by 14 numbers",
     },
   ],
   NM: [
     {
       regex: /^[0-9]{8,9}$/i,
-      description: 'License must be 8-9 numbers',
+      description: "License must be 8-9 numbers",
     },
   ],
   NY: [
     {
       regex: /^[A-Z]{1}[0-9]{7}$/i,
-      description: 'License must be 1 letter followed by 7 numbers',
+      description: "License must be 1 letter followed by 7 numbers",
+    },
+    {
+      regex: /^[A-Z]{1}[0-9]{17,18}$/i,
+      description: "License must be 1 letter followed by 17-18 numbers",
     },
     {
       regex: /^[0-9]{8,9}$/i,
-      description: 'License must be 8-9 numbers',
+      description: "License must be 8-9 numbers",
     },
   ],
   NC: [
     {
       regex: /^[0-9]{1,12}$/i,
-      description: 'License must be 1-12 numbers',
+      description: "License must be 1-12 numbers",
     },
   ],
   ND: [
     {
       regex: /^[A-Z]{3}[0-9]{6}$/i,
-      description: 'License must be 3 letters followed by 6 numbers',
+      description: "License must be 3 letters followed by 6 numbers",
     },
     {
       regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   OH: [
     {
       regex: /^[A-Z]{2}[0-9]{6}$/i,
-      description: 'License must be 2 letters followed by 6 numbers',
+      description: "License must be 2 letters followed by 6 numbers",
     },
   ],
   OK: [
     {
       regex: /^[A-Z]{1}[0-9]{9}$/i,
-      description: 'License must be 1 letter followed by 9 numbers',
-    },
-    {
-      regex: /^[0-9]{9}$/i,
-      description: 'License must be 9 numbers',
+      description: "License must be 1 letter followed by 9 numbers",
     },
     {
       regex: /^\d{7,9}$/,
-      description: 'NNNNNNNNN MinLength = 7 MaxLength = 9'
+      description: "NNNNNNNNN MinLength = 7 MaxLength = 9",
     },
   ],
   OR: [
     {
       regex: /^[0-9]{5,9}$/i,
-      description: 'License must be 5-9 numbers',
+      description: "License must be 5-9 numbers",
     },
     {
       regex: /^[A-Z]\d{7}$/i,
-      description: 'ANNNNNNN MinLength = 8 MaxLength = 8'
+      description: "ANNNNNNN MinLength = 8 MaxLength = 8",
     },
     {
       regex: /^[A-Z0-9]{6,9}$/i,
-      description: 'XXXXXXXXX MinLength = 6 MaxLength = 9'
+      description: "XXXXXXXXX MinLength = 6 MaxLength = 9",
     },
   ],
   PA: [
     {
       regex: /^[0-9]{8}$/i,
-      description: 'License must be 8 numbers',
-    }
+      description: "License must be 8 numbers",
+    },
   ],
   RI: [
     {
       regex: /^[0-9]{7,8}$/i,
-      description: 'License must be 7 or 8 numbers without letters',
+      description: "License must be 7 or 8 numbers without letters",
     },
     {
       regex: /^[A-Z]{1}[0-9]{6}$/i,
-      description: 'License must be 1 letter followed by 6 numbers',
+      description: "License must be 1 letter followed by 6 numbers",
     },
   ],
   SC: [
     {
-      regex: /^[0-9]{5,11}$/i,
-      description: 'License must be 5-11 numbers',
-    }
+      regex: /^[0-9]{5,10}$/i,
+      description: "License must be 5-11 numbers",
+    },
   ],
   SD: [
     {
       regex: /^[0-9]{6,9}$/i,
-      description: 'License must be 6-9 numbers',
+      description: "License must be 6-9 numbers",
     },
   ],
   TN: [
     {
       regex: /^[0-9]{7,9}$/i,
-      description: 'License must be 7-9 numbers',
+      description: "License must be 7-9 numbers",
     },
   ],
   TX: [
     {
       regex: /^\d{5,8}$/,
-      description: 'NNNNNNNN MinLength = 5 MaxLength = 8'
+      description: "NNNNNNNN MinLength = 5 MaxLength = 8",
     },
   ],
   UT: [
     {
       regex: /^[0-9]{4,10}$/i,
-      description: 'License must be 4-10 numbers'
-    }
+      description: "License must be 4-10 numbers",
+    },
   ],
   VT: [
     {
-      regex: /^[0-9]{8}$/i,
-      description: 'License must be 8 numbers',
+      regex: /^[0-9]{7,}$/i,
+      description: "License must be 7-8 numbers",
     },
     {
       regex: /^[0-9]{7}[A]$/i,
@@ -421,77 +432,80 @@ module.exports = {
     },
     {
       regex: /^\d{7}[A-Z]?$/i,
-      description: 'NNNNNNNA MinLength = 8 MaxLength = 8 OR NNNNNNN MinLength = 7 MaxLength = 7'
+      description:
+        "NNNNNNNA MinLength = 8 MaxLength = 8 OR NNNNNNN MinLength = 7 MaxLength = 7",
     },
   ],
   VA: [
     {
       regex: /^[A-Z]{1}[0-9]{8}$/i,
-      description: 'License must be 1 letter followed by 8 numbers',
+      description: "License must be 1 letter followed by 8 numbers",
     },
     {
       regex: /^[0-9]{9}$/,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   VI: [
     {
       regex: /^\d{10}$/,
-      description: 'NNNNNNNNNN MinLength = 10 MaxLength = 10',
+      description: "NNNNNNNNNN MinLength = 10 MaxLength = 10",
     },
   ],
   WA: [
     {
       regex: /^(?=.{12}$)[A-Z]{1,7}[A-Z0-9\\*]{4,11}$/i,
-      description: 'License must be 1-7 letters followed by any combination of letters, numbers, or "*" for a total of 12 characters',
+      description:
+        'License must be 1-7 letters followed by any combination of letters, numbers, or "*" for a total of 12 characters',
     },
     {
       regex: /^[A-Z0-9]{12}$/i,
-      description: 'XXXXXXXXXXXX MinLength = 12 MaxLength = 12'
+      description: "XXXXXXXXXXXX MinLength = 12 MaxLength = 12",
     },
   ],
   WV: [
     {
       regex: /^[0-9]{7}$/i,
-      description: 'License must be 7 numbers',
+      description: "License must be 7 numbers",
     },
     {
       regex: /^[A-Z]{2}[0-9]{5}$/i,
-      description: 'License must be 2 letters followed by 5 numbers',
+      description: "License must be 2 letters followed by 5 numbers",
     },
     {
       regex: /^[A-Z]{1,2}[0-9]{6}$/i,
-      description: 'License must be 1-2 letters followed by 6 numbers',
+      description: "License must be 1-2 letters followed by 6 numbers",
     },
     {
       regex: /^[A-Z]\d{6}$/i,
-      description: 'ANNNNNN MinLength = 7 MaxLength = 7'
+      description: "ANNNNNN MinLength = 7 MaxLength = 7",
     },
     {
       regex: /^[A-Z]{2}\d{5,6}$/i,
-      description: 'AANNNNN MinLength = 7 MaxLength = 7 OR AANNNNNN MinLength = 8 MaxLength = 8'
+      description:
+        "AANNNNN MinLength = 7 MaxLength = 7 OR AANNNNNN MinLength = 8 MaxLength = 8",
     },
     {
       regex: /^\d[A-Z]\d{5}$/i,
-      description: 'NANNNNN MinLength = 7 MaxLength = 7'
+      description: "NANNNNN MinLength = 7 MaxLength = 7",
     },
   ],
   WI: [
     {
       regex: /^[A-Z]{1}[0-9]{13}$/i,
-      description: 'License must be 1 letter followed by 13 numbers',
+      description: "License must be 1 letter followed by 13 numbers",
     },
   ],
   WY: [
     {
       regex: /^[0-9]{9}$/,
-      description: 'License must be 9 numbers',
+      description: "License must be 9 numbers",
     },
   ],
   PR: [
     {
       regex: /^\d{7,9}$/,
-      description: 'NNNNNNNNN MinLength = 7 MaxLength = 9'
-    }
-  ]
+      description: "NNNNNNNNN MinLength = 7 MaxLength = 9",
+    },
+  ],
 };
